469c23862e9dfeb0e755e11d04b2b1cb65823101
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2025-03-05 20:07:25 +0700

    refactor(CrudController): rename generic ID to I to follow SonarQube naming convention

--- src/main/java/id/ac/ui/cs/advprog/eshop/contoller/CrudController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/contoller/CrudController.java
@@ -2,11 +2,11 @@ package id.ac.ui.cs.advprog.eshop.contoller;
 
 import org.springframework.ui.Model;
 
-public interface CrudController<T, ID> {
+public interface CrudController<T, I> {
     String getCreatePage(Model model);
     String postCreate(T entity, Model model);
     String getListPage(Model model);
-    String getEditPage(ID id, Model model);
-    String postUpdate(ID id, T entity, Model model);
-    String postDelete(ID id);
+    String getEditPage(I id, Model model);
+    String postUpdate(I id, T entity, Model model);
+    String postDelete(I id);
 }
