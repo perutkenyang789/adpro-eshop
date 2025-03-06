2a8ab9603eb6203792147625e6b268b37ba849ef
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2025-03-07 01:53:11 +0700

    [REFACTOR] Remove lombok builder from model class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -2,20 +2,18 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
-import lombok.Builder;
 import lombok.Getter;
 
 import java.util.Map;
 
-@Builder
 @Getter
 public class Payment {
     private String id;
     private PaymentMethod method;
     private PaymentStatus status;
-    private Map<String, String> paymentData;
+    private Map<String,String> paymentData;
 
-    public Payment(String id, PaymentMethod method, Map<String, String> paymentData) {
+    public Payment(String id, PaymentMethod method, Map<String,String> paymentData) {
         this.id = id;
         this.method = method;
         this.paymentData = paymentData;
@@ -26,11 +24,6 @@ public class Payment {
         }
 
         validateAndSetStatus();
-    }
-
-    public Payment(String id, PaymentMethod method, Map<String, String> paymentData, PaymentStatus status) {
-        this(id, method, paymentData);
-        setStatus(status);
     }
 
     public void setStatus(PaymentStatus status) {
