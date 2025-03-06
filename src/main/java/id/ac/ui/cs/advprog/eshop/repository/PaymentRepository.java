c271e4746d962cf22076f477b024a8673a4d1da2
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2025-03-07 01:58:13 +0700

    [GREEN] Implement PaymentRepository class

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class PaymentRepository {
+    private Map<Order, Payment> orderPayments = new HashMap<Order, Payment>();
+
+    public Payment save(Order order, Payment payment) {
+        orderPayments.put(order, payment);
+        return payment;
+    }
+
+    public Payment findByPaymentId(String orderId) {
+        for (Order order : orderPayments.keySet()) {
+            if (order.getId().equals(orderId)) {
+                return orderPayments.get(order);
+            }
+        }
+        return null;
+    }
+
+    public List<Payment> findAll() {
+        return List.copyOf(orderPayments.values());
+    }
+}
