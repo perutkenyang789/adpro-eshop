46fef9f316ce0da8d4f1ded48e5da82be2885427
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2025-03-07 13:26:48 +0700

    [REFACTOR] Remove unused import in OrderTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -3,7 +3,6 @@ package id.ac.ui.cs.advprog.eshop.model;
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.internal.matchers.Or;
 
 import java.util.ArrayList;
 import java.util.List;
